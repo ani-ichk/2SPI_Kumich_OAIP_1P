2f9d2cf38f9d305cf24b14c5b1995e9fcf1aa29e
ani-ichk <akumic8400@gmail.com>
2025-01-31 00:16:52 +0900

    Update func_table.py

--- 2SPI_Kumich_4L/func_table.py
+++ 2SPI_Kumich_4L/func_table.py
@@ -1,9 +1,9 @@
 def func_table(f, x_max, y_max):
     # цикл, проходящий по всем возможным значениям y
-    for y in range(y_max + 1):
+    for x in range(x_max + 1):
         res = []
         # цикл, проходящий по всем возможным значениям x
-        for x in range(x_max + 1):
+        for y in range(y_max + 1):
             # вычисляем значение функции и доб-ем в список-результат
             res.append(str(eval(f)))
         print('\t'.join(res))
